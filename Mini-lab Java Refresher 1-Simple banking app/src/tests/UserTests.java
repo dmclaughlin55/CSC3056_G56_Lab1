7677ca6de268e8e4b67f8b28ead1fdfb1c170e06
40265925 <jrajan01@qub.ac.uk>
2023-02-07 15:10:07 +0000

    userTests development

--- Mini-lab Java Refresher 1-Simple banking app/src/tests/UserTests.java
+++ Mini-lab Java Refresher 1-Simple banking app/src/tests/UserTests.java
@@ -4,6 +4,12 @@ public class UserTests {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		public static void main(String[] args) {
+			User testUser = new User("mike", "my_passwd", "Mike", "Smith", "07771234567");
+			
+			System.out.println(testUser);
+		}
 
 	}
 

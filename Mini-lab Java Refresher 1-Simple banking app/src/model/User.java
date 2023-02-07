5272f4f15c6ff25e15df2f6428c1d479a062acda
40265925 <jrajan01@qub.ac.uk>
2023-02-07 16:29:47 +0000

    TODO 1 Complete
    automated testing

--- Mini-lab Java Refresher 1-Simple banking app/src/model/User.java
+++ Mini-lab Java Refresher 1-Simple banking app/src/model/User.java
@@ -46,4 +46,9 @@ public class User {
 	public void setMobile_number(String mobile_number) {
 		this.mobile_number = mobile_number;
 	}
+	
+	public String toString() {
+		return username +", " + password + ", " + first_name + ", " + last_name + ", " + mobile_number;
+		
+	}
 }
